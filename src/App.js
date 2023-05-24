683790cc1f5343b978d02aa7607931d254a3f10c
Joseph Noblett <noblettjoe@gmail.com>
2023-05-23 22:03:25 -0500

    make basic layout/structure of web App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <nav>
+        <img src={'./logo192.png'} width={'40px'} alt={"Javascript React logo made by facebook"} />
+      </nav> 
+      <h1>Fun Facts About React</h1>
+      <ul>
+        <li>Was released in 2013</li>
+        <li>Was Originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>is maintained by facebook and the Github community</li>
+        <li>Powers thousands of enterprise apps, including but not limited to mobile apps</li>
+      </ul>
+    </div>
   );
 }
 
